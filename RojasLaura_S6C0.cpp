fb85be019878f81c3467101572e367d2998de7d4
Laura Rojas <ca.triana957@uniandes.edu.co>
2018-07-09 09:24:50 -0500

    RojasLaura_S6C0.cpp

---
+++ RojasLaura_S6C0.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+double f(double x, double y[], int i);     //Funcion para las derivadas 
+
+ y[0] = 1.0
+ x  
+
+int dydx(){
+  ;
+}
+
+int main(){
+  
+int N,h;
+
+  N = 1000.0;
+  h = 0,1;
+ 
+ for (i=1;i<N;i++);{
+
+ y[0] = 1.0
+ y[i+1] = (h+1)*y[i];
+ y[i] = y[i-1] + h * y[i](x[i-1],y[i-1]
+
+
+
+}
+
+
+ return 0;
+}
+
